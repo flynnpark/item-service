bd0216fed97ca9d5f2477473540f46125b8126f6
Flynn <wphestiraid@gmail.com>
2025-01-02 09:05:59 +0900

    스프링 MVC: 웹 페이지 만들기 - 프로젝트 생성

---
+++ src/test/java/dev/flynnpark/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flynnpark.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
